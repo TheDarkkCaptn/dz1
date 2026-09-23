09682bacf7ba05f6e8dabb97148605dcbba382c2
TheDarkkCaptn <duxhus@gmail.com>
2026-09-23 17:02:50 +0300

    Update TicketService.java

--- src/service/TicketService.java
+++ src/service/TicketService.java
@@ -1,5 +1,6 @@
 package service;
 
+import model.NotificationService;
 import model.Ticket;
 import model.TicketStatus;
 
@@ -15,6 +16,24 @@ public class TicketService {
         if (ticket.getStatus() == TicketStatus.IN_PROGRESS) {
             notificationService.send(
                     "Заявка №" + ticket.getId() + " принята в работу"
+            );
+        }
+    }
+
+    public void resolveTicket(Ticket ticket) {
+        ticket.resolve();
+        if (ticket.getStatus() == TicketStatus.RESOLVED) {
+            notificationService.send(
+                    "По заявке №" + ticket.getId() + " найдено решение"
+            );
+        }
+    }
+
+    public void closeTicket(Ticket ticket) {
+        ticket.close();
+        if (ticket.getStatus() == TicketStatus.CLOSED) {
+            notificationService.send(
+                    "Заявка №" + ticket.getId() + " закрыта"
             );
         }
     }
